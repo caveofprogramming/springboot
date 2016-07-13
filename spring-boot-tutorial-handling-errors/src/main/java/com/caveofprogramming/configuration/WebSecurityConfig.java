38159385435e2b400217c29271520bdd780b5492
John Purcell <johnpurcell@Johns-iMac.home>
2016-07-13 21:48:15 +0100

    Added

---
+++ spring-boot-tutorial-handling-errors/src/main/java/com/caveofprogramming/configuration/WebSecurityConfig.java
@@ -0,0 +1,85 @@
+package com.caveofprogramming.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.caveofprogramming.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		// @formatter:off
+		
+		http
+			.authorizeRequests()
+				.antMatchers(
+						"/",
+						"/about",
+						"/register",
+						"/registrationconfirmed",
+						"/invaliduser",
+						"/expiredtoken",
+						"/verifyemail",
+						"/confirmregister"
+						)
+				.permitAll()
+				.antMatchers(
+					"/js/*",
+					"/css/*",
+					"/img/*")
+				.permitAll()
+				.antMatchers("/addstatus",
+						"/editstatus",
+						"/deletestatus",
+						"/viewstatus")
+				.hasRole("ADMIN")
+				.anyRequest()
+				.denyAll()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+		
+		// @formatter:on
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// @formatter:off
+		auth
+			.inMemoryAuthentication()
+			.withUser("john")
+			.password("hello")
+			.roles("USER");
+		
+		// @formatter:on
+
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+	}
+
+	
+}
