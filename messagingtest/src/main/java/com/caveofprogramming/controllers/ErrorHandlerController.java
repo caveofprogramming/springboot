5e5f345e1b557fc27782cbbd9e6d3731b18984df
John Purcell <john@caveofprogramming.com>
2018-06-17 04:27:12 +0100

    updated

---
+++ messagingtest/src/main/java/com/caveofprogramming/controllers/ErrorHandlerController.java
@@ -0,0 +1,31 @@
+package com.caveofprogramming.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorHandlerController implements ErrorController {
+
+	@RequestMapping("/error")
+	public ModelAndView errorHandler(HttpServletRequest req) {
+
+		ModelAndView modelAndView = new ModelAndView();
+
+		modelAndView.getModel().put("message", "Unknown error");
+		modelAndView.getModel().put("url", req.getRequestURL());
+
+		modelAndView.setViewName("app.exception");
+
+		return modelAndView;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+}
