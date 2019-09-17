a6d2ae3779db7ff7af4e7e7bb52ec2d137188949
John Purcell <john.purcell@caveofprogramming.com>
2019-09-17 20:07:48 +0200

    Fixed message queue

--- spring-boot-tutorial-connection-manager/src/main/java/com/caveofprogramming/controllers/ChatController.java
+++ spring-boot-tutorial-connection-manager/src/main/java/com/caveofprogramming/controllers/ChatController.java
@@ -1,6 +1,7 @@
 package com.caveofprogramming.controllers;
 
 import java.security.Principal;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -46,12 +47,32 @@ public class ChatController {
 	public void send(Principal principal, SimpleMessage message, @DestinationVariable Long toUserId) {
 		System.out.println(message);
 		
+		// Get details for sending user (current user)
 		String fromUsername = principal.getName();
 		SiteUser fromUser = userService.get(fromUsername);
 		Long fromUserId = fromUser.getId();
 		
+		// Get details for user we are chatting with.
+		Optional<SiteUser> toUserOpt = userService.get(toUserId);
+		SiteUser toUser = toUserOpt.get();
+		String toUsername = toUser.getEmail();
+		
 		String returnReceiptQueue = String.format("/queue/%d", toUserId);
+		String toUserQueue = String.format("/queue/%d", fromUserId);
 		
 		simpleMessagingTemplate.convertAndSendToUser(fromUsername, returnReceiptQueue, message);
+		simpleMessagingTemplate.convertAndSendToUser(toUsername, toUserQueue, message);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
