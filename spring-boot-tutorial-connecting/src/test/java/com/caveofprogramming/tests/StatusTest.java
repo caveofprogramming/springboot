a0e7bd08c5be654738d6976969b868fd770c657e
John Purcell <john.purcell@caveofprogramming.com>
2019-10-16 15:05:04 +0200

    Upgraded to 2.1.9

--- spring-boot-tutorial-connecting/src/test/java/com/caveofprogramming/tests/StatusTest.java
+++ spring-boot-tutorial-connecting/src/test/java/com/caveofprogramming/tests/StatusTest.java
@@ -1,23 +1,23 @@
 package com.caveofprogramming.tests;
 
 import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.caveofprogramming.App;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(App.class)
-@WebAppConfiguration
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
 public class StatusTest {
 
 	@Test
 	public void testDummy() {
 		Long value = 7L;
-		
+
 		assertNotNull("Value should not be null", value);
 	}
 }
