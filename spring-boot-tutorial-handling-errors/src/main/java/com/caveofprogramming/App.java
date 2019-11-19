e16478fe1070f205d3f3286485964904276304b8
John Purcell <john.purcell@caveofprogramming.com>
2019-11-19 21:15:00 +0100

    Updated

--- spring-boot-tutorial-handling-errors/src/main/java/com/caveofprogramming/App.java
+++ spring-boot-tutorial-handling-errors/src/main/java/com/caveofprogramming/App.java
@@ -24,45 +24,44 @@ public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(App.class);
 	}
-	
+
 	@Bean
 	public TilesConfigurer tilesConfigurer() {
 		TilesConfigurer tilesConfigurer = new TilesConfigurer();
-		
-		String[] defs = {"/WEB-INF/tiles.xml"};
-		
+
+		String[] defs = { "/WEB-INF/tiles.xml" };
+
 		tilesConfigurer.setDefinitions(defs);
-		
+
 		return tilesConfigurer;
 	}
-	
+
 	@Bean
 	public UrlBasedViewResolver tilesViewResolver() {
 		UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();
 		tilesViewResolver.setViewClass(TilesView.class);
 		return tilesViewResolver;
 	}
-	
+
 	@Bean
 	PasswordEncoder getEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	 @Configuration
-     public class ServerConfig {
-             @Bean
-             public ConfigurableServletWebServerFactory webServerFactory() {
-                     TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-                     
-                     factory.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/403"));
-                     return factory;
-             }
-     }
 
+	@Configuration
+	public class ServerConfig {
+		@Bean
+		public ConfigurableServletWebServerFactory webServerFactory() {
+			TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+
+			factory.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/403"));
+			return factory;
+		}
+	}
 
 }
